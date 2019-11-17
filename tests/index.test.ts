cabbbbf687ccd81ce25ce2e9367355d603da07b4
Stephen Vector <stephenvector@users.noreply.github.com>
2019-11-17 18:43:50 -0500

    Fixing typo

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
 import { getRowBoxColumn } from "../src";
 
 test("Should return 0 for row, box, & column for square at index 0", () => {
-  const { row, box, column } = getRowBoxColumn(0,0);
+  const { row, box, column } = getRowBoxColumn(0,0,0);
   expect(row).toBe(0);
   expect(box).toBe(0);
   expect(column).toBe(0);
