a24373aa0670bdf318bcf5fc8ff071e2423ab928
Stephen Vector <stephenvector@users.noreply.github.com>
2019-11-17 18:52:52 -0500

    Adding getPossibilities() function.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ export function getSquareRowBoxColumn(squareIndex: number, n: number) {
   const box = Math.floor(row/n) * n + Math.floor(column/n);
   return { row, col, box }
 }
+
+export function getPossibilities(n: number): number[] {
+  const possibilities: number[] = [];
+  for (let i=0; i< n * n; n++) {
+    possibilities.push(i + 1);
+  }
+  return possibilities;
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,12 @@
-import { getRowBoxColumn } from "../src";
+import { getRowBoxColumn, getPossibilities } from "../src";
 
 test("Should return 0 for row, box, & column for square at index 0", () => {
   const { row, box, column } = getRowBoxColumn(0,0,0);
   expect(row).toBe(0);
   expect(box).toBe(0);
   expect(column).toBe(0);
+});
+
+test("Should return [1,2,3,4] from getPossibilities(2)", () => {
+  expect(getPossibilities(2)).toBe([1,2,3,4]);
 });
