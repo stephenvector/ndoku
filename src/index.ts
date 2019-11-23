684fd99ad66fe4993aabb58d52fc91651cb47ea1
Stephen Vector <stephenvector@pm.me>
2019-11-22 21:49:25 -0500

    Generating complete puzzles for n=1 & n=2.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+type Possibilities = {
+  [squareIndex: number]: number[];
+};
+
 export function getSquareRowBoxColumn(squareIndex: number, n: number) {
   const numPerRow = n * n;
   const row = Math.floor(squareIndex / numPerRow);
@@ -6,10 +10,105 @@ export function getSquareRowBoxColumn(squareIndex: number, n: number) {
   return { row, column, box };
 }
 
+export function removeValueFromPossibilities(
+  value: number,
+  squareIndex: number,
+  possibilities: Possibilities,
+  n: number
+) {
+  const { row, box, column } = getSquareRowBoxColumn(squareIndex, n);
+  Object.keys(possibilities).forEach(blankIndex => {
+    const blankRowBoxColumn = getSquareRowBoxColumn(parseInt(blankIndex), n);
+    if (
+      row === blankRowBoxColumn.row ||
+      box === blankRowBoxColumn.box ||
+      column === blankRowBoxColumn.column
+    ) {
+      const valueIndex = possibilities[blankIndex].indexOf(value);
+
+      if (valueIndex !== -1) {
+        possibilities[blankIndex] = [
+          ...possibilities[blankIndex].slice(0, valueIndex),
+          ...possibilities[blankIndex].slice(valueIndex + 1)
+        ];
+      }
+    }
+  });
+
+  return possibilities;
+}
+
 export function getPossibilities(n: number): number[] {
   const possibilities: number[] = [];
   for (let i = 0; i < n * n; i++) {
     possibilities.push(i + 1);
   }
   return possibilities;
+}
+
+export function getNumBlanks(puzzle: number[]): number {
+  return puzzle.filter(square => square === 0).length;
+}
+
+export function getSquareIndexesWithFewestPossibilities(
+  p: Possibilities,
+  n: number
+): number[] {
+  const squareIndexes = [];
+
+  // Find lowest number of possibilities that exists in the puzzle
+  let lowestNumberOfPossibilities = n * n;
+  Object.keys(p).forEach(squareIndex => {
+    const numPossible = p[squareIndex].length;
+    if (numPossible < lowestNumberOfPossibilities) {
+      lowestNumberOfPossibilities = numPossible;
+    }
+  });
+
+  Object.keys(p).forEach(squareIndex => {
+    if (p[squareIndex].length === lowestNumberOfPossibilities) {
+      squareIndexes.push(parseInt(squareIndex));
+    }
+  });
+
+  return squareIndexes;
+}
+
+export function createCompletedPuzzle(n: number) {
+  const totalNumSquares = n * n * (n * n);
+  const puzzle: number[] = Array(totalNumSquares).fill(0);
+  let possibilities: Possibilities = {};
+  let numBlanks = totalNumSquares;
+
+  // Initialize possibilities
+  for (let i = 0; i < totalNumSquares; i++) {
+    possibilities[i] = getPossibilities(n);
+  }
+
+  while (numBlanks > 0) {
+    const blanks = getSquareIndexesWithFewestPossibilities(possibilities, n);
+
+    const randomBlankSquareIndex =
+      blanks[Math.floor(Math.random() * blanks.length)];
+
+    const randomValue =
+      possibilities[randomBlankSquareIndex][
+        Math.floor(Math.random() * possibilities[randomBlankSquareIndex].length)
+      ];
+
+    puzzle[randomBlankSquareIndex] = randomValue;
+
+    possibilities = removeValueFromPossibilities(
+      puzzle[randomBlankSquareIndex],
+      randomBlankSquareIndex,
+      possibilities,
+      n
+    );
+
+    delete possibilities[randomBlankSquareIndex];
+
+    numBlanks = getNumBlanks(puzzle);
+  }
+
+  return puzzle;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,10 @@
-import { getSquareRowBoxColumn, getPossibilities } from "../src";
+import {
+  getSquareRowBoxColumn,
+  getPossibilities,
+  createCompletedPuzzle
+} from "../src";
+
+// getSquareRowBoxColumn
 
 test("Should return 0 for row, box, & column for square at index 0", () => {
   const { row, box, column } = getSquareRowBoxColumn(0, 3);
@@ -7,6 +13,26 @@ test("Should return 0 for row, box, & column for square at index 0", () => {
   expect(column).toBe(0);
 });
 
+// getPossibilities
+
+test("Should return [1] from getPossibilities(1)", () => {
+  expect(getPossibilities(1)).toStrictEqual([1]);
+});
+
 test("Should return [1,2,3,4] from getPossibilities(2)", () => {
   expect(getPossibilities(2)).toStrictEqual([1, 2, 3, 4]);
 });
+
+test("Should return [1,2,3,4,5,6,7,8,9] from getPossibilities(3)", () => {
+  expect(getPossibilities(3)).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+
+// createCompletedPuzzle
+
+test("Should return [1] from createCompletedPuzzle(1)", () => {
+  expect(createCompletedPuzzle(1)).toStrictEqual([1]);
+});
+
+const t = createCompletedPuzzle(3);
+
+console.log(JSON.stringify(t));
