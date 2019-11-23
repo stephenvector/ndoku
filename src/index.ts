dc6485f7a58f8cc1bfdb6fecaef50ce566a69bab
Stephen Vector <stephenvector@pm.me>
2019-11-22 19:10:13 -0500

    Fixing testing suite.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 export function getSquareRowBoxColumn(squareIndex: number, n: number) {
   const numPerRow = n * n;
-  const row = Math.floor(squareNumber / numPerRow);
-  const column = squareNumber % numPerRow;
-  const box = Math.floor(row/n) * n + Math.floor(column/n);
-  return { row, col, box }
+  const row = Math.floor(squareIndex / numPerRow);
+  const column = squareIndex % numPerRow;
+  const box = Math.floor(row / n) * n + Math.floor(column / n);
+  return { row, column, box };
 }
 
 export function getPossibilities(n: number): number[] {
   const possibilities: number[] = [];
-  for (let i=0; i< n * n; n++) {
+  for (let i = 0; i < n * n; i++) {
     possibilities.push(i + 1);
   }
   return possibilities;
